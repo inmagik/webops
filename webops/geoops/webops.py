ea95feb118f9502432c1ce02de2f9764a43583bc
francot <frenk.calza@gmail.com>
2015-02-18 17:53:34 +0100

    add webops edit

--- webops/geoops/webops.py
+++ webops/geoops/webops.py
@@ -2,9 +2,11 @@ from django.apps import AppConfig
 from opsmanager.register import _register
 from .ops.ogr2ogr import OgrOp
 from .ops.buffer import BufferOp
+from .ops.intersection import IntersectionOp
+
 from .ops.gdaltranslate import GDALTranslateOp
 
 _register.register_op(OgrOp)
 _register.register_op(GDALTranslateOp)
 _register.register_op(BufferOp)
-
+_register.register_op(IntersectionOp)
